d0ef42dae47d364e5cc543b59978916115b7b156
Kyle Johnson <miningape@gmail.com>
2020-06-08 22:52:29 +0200

    Fixed bug where it did multiple lines for normal output

--- js/brainfuck.js
+++ js/brainfuck.js
@@ -53,7 +53,6 @@ class BFInterpreter {
                     break;
                 case '.': // Write data at the current cell to the terminal
                     this.printfn.call(this, this.ASCII(this.environment[this.pointer]));
-                    this.printfn.call(this, '\n');
                     break;
                 case '^': // Breakpoint: Outputs environment
                     this.printfn.call(this, this.environment);
